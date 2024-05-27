b703a1a429702c8f9da9f3bcd9a80ddb45f65705
Goutam Vankudre <79298277+vankudregoutam@users.noreply.github.com>
2024-05-27 10:48:40 +0530

    first commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -55,7 +55,7 @@ export default function TextForm(props) {
                 <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Change to Lower Case</button>
                 <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClear}>Clear Text</button>
                 <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
-                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Revome Extra Spaces</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             </div>
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Your text summary</h2>
